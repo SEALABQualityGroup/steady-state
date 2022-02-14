e2e3de5f2861b1b04e4a8e85a6203be2d04bd6c2
lucatraini <lucatra89@gmail.com>
2022-02-14 12:28:55 +0100

    fix

--- changepoint_analysis.py
+++ changepoint_analysis.py
@@ -6,19 +6,24 @@ filtered_dirpath = './data/timeseries/filtered'
 jmh_dirpath = './data/timeseries/all'
 changepoints_dirpath = './data/changepoints'
 
-if not os.path.exists(changepoints_dirpath):
-    os.mkdir(changepoints_dirpath)
+def changepoint_analysis():
+    if not os.path.exists(changepoints_dirpath):
+        os.mkdir(changepoints_dirpath)
 
-for filtered_path in glob('{}/*.json'.format(filtered_dirpath)):
+    for filtered_path in glob('{}/*.json'.format(filtered_dirpath)):
 
-    print(filtered_path)
-    filename = filtered_path.split('/')[-1]
-    jmh_path = '{}/{}'.format(jmh_dirpath, filename)
-    changepoints_path = '{}/{}'.format(changepoints_dirpath, filename)
+        print(filtered_path)
+        filename = filtered_path.split('/')[-1]
+        jmh_path = '{}/{}'.format(jmh_dirpath, filename)
+        changepoints_path = '{}/{}'.format(changepoints_dirpath, filename)
 
-    jmh_path, filtered_path, changepoints_path = [path for path in [jmh_path, filtered_path, changepoints_path]]
-    if not os.path.exists(changepoints_path):
-        print('Executing ', filename, '..')
-        changepoint(jmh_path, filtered_path, changepoints_path)
-    else:
-        print(filename, 'already exists')
+        jmh_path, filtered_path, changepoints_path = [path for path in [jmh_path, filtered_path, changepoints_path]]
+        if not os.path.exists(changepoints_path):
+            print('Executing ', filename, '..')
+            changepoint(jmh_path, filtered_path, changepoints_path)
+        else:
+            print(filename, 'already exists')
+
+
+if __name__ == '__main__':
+    changepoint_analysis()
--- steadystate.py
+++ steadystate.py
@@ -1,8 +1,11 @@
 from create_timeseries import create_timeseries
 from filter_outliers import filter_outliers
+from changepoint_analysis import changepoint_analysis
 from classify_runs import classify_runs
+
 
 if __name__ == '__main__':
     create_timeseries()
     filter_outliers()
+    changepoint_analysis()
     classify_runs()
